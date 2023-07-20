ebd3f3a07990f592691a1c4811f4459db0639401
Abhishek Gupta <bsaagupta77@gmail.com>
2023-07-20 18:32:43 +0530

    first commit

---
+++ src/components/footer/footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './footer.css';
+function Footer() {
+  return (
+    <div className='bl'>
+     <footer>Made with ❤️ by Abhishek Gupta,2023</footer> 
+    </div>
+  )
+}
+
+export default Footer
